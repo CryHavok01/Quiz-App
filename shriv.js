3501f65a2f9a84dcc8b93e90bf0805288cc0cf84
CryHavok01 <jhhalpern@gmail.com>
2021-03-02 17:22:57 -0500

    fixed pics

--- shriv.js
+++ shriv.js
@@ -14,7 +14,7 @@ class Shriv extends React.Component {
         return (
             <div>
                 <h2>You're Shriv Suurgav</h2>
-                <img src="/pics/Shriv.jpg" alt="Shriv Suurgav" />
+                <img src="pics/Shriv.jpg" alt="Shriv Suurgav" />
                 <p>
                     Shriv Suurgav was a soldier and strategist for the Rebel Alliance, who fought in several key battles of the Galactic Civil War.  Although he had a constantly sarcastic demeanor and cynical attitude, he was nevetherless a loyal soldier and reliable friend to General Lando Calrissian and other notable Rebels.  He fought in the liberation of Sullust and at the Battle of Jakku, although he spent as much time complaining to his comrades as he did fighting the Empire.
                 </p>
