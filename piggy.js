79c854c8b06a329911034969ab51972d1dcd01bb
CryHavok01 <jhhalpern@gmail.com>
2021-03-03 16:02:47 -0500

    img center test

--- piggy.js
+++ piggy.js
@@ -20,7 +20,7 @@ class Piggy extends React.Component {
                 </div>
                 <div className="row">
                     <div className="col">
-                        <img src="pics/Piggy.jpg" className="img-fluid" alt="Voort saBinring" />                        
+                        <img src="pics/Piggy.jpg" className="img-fluid mx-auto" alt="Voort saBinring" />                        
                     </div>
                 </div>
                 <div className="row">
